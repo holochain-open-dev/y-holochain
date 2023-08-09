66fd9cd912105f62efa0f8f988028cf1a40d8f82
Matt Gabrenya <matt@buildyourweb.app>
2023-08-08 17:14:43 -0700

    feat: add demo for TipTap editor

--- tests/demo/ui/src/main.ts
+++ tests/demo/ui/src/main.ts
@@ -2,16 +2,23 @@ import { createApp } from "vue";
 import "./style.css";
 import "./assets/quill.snow.css";
 import { createRouter, createWebHashHistory } from "vue-router";
-import DocumentDetail from "./demo/yjs/DocumentDetail.vue";
+import QuillDocumentDetail from "./demo/yjs/QuillDocumentDetail.vue";
+import TiptapDocumentDetail from "./demo/yjs/TiptapDocumentDetail.vue";
 import AllDocuments from "./demo/yjs/AllDocuments.vue";
 import App from "./App.vue";
 
 const routes = [
   { name: "home", path: "/", component: AllDocuments },
   {
-    name: "document",
-    path: "/document/:documentHashB64",
-    component: DocumentDetail,
+    name: "document/quill",
+    path: "/document/quill/:documentHashB64",
+    component: QuillDocumentDetail,
+    props: true,
+  },
+  {
+    name: "document/tiptap",
+    path: "/document/tiptap/:documentHashB64",
+    component: TiptapDocumentDetail,
     props: true,
   },
 ];
